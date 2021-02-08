9b5c9785a846bbe15435ae26f1a264058729077d
Ryan Rudes <ryanrudes@gmail.com>
2021-02-08 10:30:59 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'colabgymrender',
   packages = ['colabgymrender'],
-  version = '1.0.1',
+  version = '1.0.2',
   license='MIT',
   description = 'A wrapper for rendering OpenAI Gym environments in Google Colab',
   author = 'Ryan Rudes',
   author_email = 'ryanrudes@gmail.com',
   url = 'https://github.com/Ryan-Rudes/colabgymrender',
-  download_url = 'https://github.com/Ryan-Rudes/colabgymrender/archive/v1.0.1.tar.gz',
+  download_url = 'https://github.com/Ryan-Rudes/colabgymrender/archive/v1.0.2.tar.gz',
   keywords = ['colab', 'gym', 'render'],
   install_requires=[
           'pyvirtualdisplay',
