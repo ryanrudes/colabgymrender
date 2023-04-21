07cdfa5274d72d9fab1f5de6642d2f7321328372
Ryan Rudes <ryanrudes@gmail.com>
2023-04-21 10:15:14 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup, find_packages
+
+import datetime
 import codecs
 import os
 
-version = os.environ.get('VERSION', '0.1.0')
+
+major = 0
+minor = 1
+
+timestamp = datetime.datetime.utcnow().strftime('%Y%m%d%H%M%S')
+version = f"{major}.{minor}.{timestamp}@dev"
+
 here = os.path.abspath(os.path.dirname(__file__))
 
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
